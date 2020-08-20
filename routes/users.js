e0cde37785fb29f03307ef761be8a05539e6e76b
Buba Mohammed Auwal <juniorbuba4real@gmail.com>
2020-08-20 03:23:47 +0100

    fix: bug squashed here too

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,20 @@
 const router = require('express').Router();
-let User = require('../model/user.model');
+let User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
-    User.find()
-        .then(users => res.json(users))
-        .catch(err => res.status(400).json('Error encountered: ' + err));
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req,res) =>{
-    const username = req.body.username;
-    const newUser = new User({username});
+router.route('/add').post((req, res) => {
+  const username = req.body.username;
 
-    newUser.save()
-    .then(() => res.json('User added'))
-    .catch(err => res.status(400).json('Error encountered: ' + err));
+  const newUser = new User({username});
+
+  newUser.save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
-module.export = router;
+module.exports = router;
