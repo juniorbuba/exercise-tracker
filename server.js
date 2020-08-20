a842345b581ceb70e77fc9312158fd1d9725c391
Buba Mohammed Auwal <juniorbuba4real@gmail.com>
2020-08-20 03:21:08 +0100

    fix: bug stopping server start squashed

--- server.js
+++ server.js
@@ -1,37 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const chalk = require('chalk');
 
 require('dotenv').config();
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const mongooseconnection = mongoose.connection;
 const uri = process.env.MONGODB_URL;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
 
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
-
-try {
-   mongooseconnection.once('open', () => {
-      console.log(chalk.green.bold('Successfully connected to database'));
-  });
-} catch (error) {
-    console.log(chalk.red.bold('Error connecting to database'));
-    console.log(error);
-}
-  
-//require and use routes on the next few lines
 const exercisesRouter = require('./routes/exercises');
-const userRouter = require('./routes/users');
+const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);
-app.use('/users', userRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
-    console.log(chalk.green.bold.inverse(`Server is running on port ${port}`));
+    console.log(`Server is running on port: ${port}`);
 });
