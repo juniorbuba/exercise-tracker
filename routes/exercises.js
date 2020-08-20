46c5c8d01c757c84e6d249154a9480791404b750
Buba Mohammed Auwal <juniorbuba4real@gmail.com>
2020-08-20 03:21:42 +0100

    fix: bug squashed here too

--- routes/exercises.js
+++ routes/exercises.js
@@ -1,35 +1,55 @@
 const router = require('express').Router();
-let Exercise = require('../model/exercise.model');
+let Exercise = require('../models/exercise.model');
 
 router.route('/').get((req, res) => {
-    Exercise.find()
-        .then(exercises => res.json(exercises))
-        .catch(err => res.status(400).json('Error encountered: ' + err));
+  Exercise.find()
+    .then(exercises => res.json(exercises))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req,res) =>{
-    const username = req.body.username;
-    const description = req.body.description;
-    const duration = Number(req.body.duration);
-    const date = Date.parse(req.body.date);
+router.route('/add').post((req, res) => {
+  const username = req.body.username;
+  const description = req.body.description;
+  const duration = Number(req.body.duration);
+  const date = Date.parse(req.body.date);
 
-    const newExercise = new Exercise({
-        username,
-        description,
-        duration,
-        date
-    });
+  const newExercise = new Exercise({
+    username,
+    description,
+    duration,
+    date,
+  });
 
-    newExercise.save()
-    .then(() => res.json('Exercise Saved'))
-    .catch(err => res.status(400).json('Error encountered: ' + err));
+  newExercise.save()
+  .then(() => res.json('Exercise added!'))
+  .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/delete').get((req, res) => {
-    Exercise.deleteOne()
-    .then(() => res.json('Exercise deleted'))
-    .catch(err => res.status(400).json('Error encountered while deleting: ' + err));
-    
+router.route('/:id').get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
-    module.export = router;
+router.route('/:id').delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Exercise deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = Number(req.body.duration);
+      exercise.date = Date.parse(req.body.date);
+
+      exercise.save()
+        .then(() => res.json('Exercise updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
